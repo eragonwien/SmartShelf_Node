e5c7c80199820297aac7f1d79e71ecd70043b27d
Nguyen <eragonwien@gmail.com>
2017-07-02 12:17:56 +0200

    add

--- sonic_measure.py
+++ sonic_measure.py
@@ -10,25 +10,22 @@ def get_sonic_value(echo, trigger):
     except ImportError:
         # if no device is found. a random number is generated for testing
         return round(random.uniform(echo, trigger), 0)
-    print('+')
+    timer_start = time.time()
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(trigger, GPIO.OUT)
     GPIO.output(trigger, 0)
     GPIO.setup(echo, GPIO.IN)
     time.sleep(0.1)
-    print('+')
     # print("Start sonic measure...")
     GPIO.output(trigger, 1)
     time.sleep(0.00001)
     GPIO.output(trigger, 0)
-    print('+')
-    timer_start = time.time()
-    while GPIO.input(echo) == 0 and (time.time() - timer_start) < 1:
+
+    while GPIO.input(echo) == 0 and (time.time() - timer_start) < 0.5:
         pass
     start = time.time()
-    while GPIO.input(echo) == 1 and (time.time() - timer_start) < 1:
+    while GPIO.input(echo) == 1 and (time.time() - timer_start) < 0.5:
         pass
     stop = time.time()
     GPIO.cleanup()
-    print('+')
     return (stop - start) * 17000
